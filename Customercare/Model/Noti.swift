211844db681603e7392d6ca614417727710ac03d
Browser <browser@demo.local>
2018-04-14 01:31:21 +0700

    The first commit and add noti, home view , controller

---
+++ Customercare/Model/Noti.swift
@@ -0,0 +1,18 @@
+//
+//  Noti.swift
+//  Customercare
+//
+//  Created by Browser on 4/14/18.
+//  Copyright © 2018 vn. All rights reserved.
+//
+
+import Foundation
+struct Noti{
+    var title: String?
+    var body : String?
+    init(title: String?, body: String?) {
+        self.title = title
+        self.body = body
+    }
+}
+
---
+++ Customercare/Model/NotiData.swift
@@ -0,0 +1,21 @@
+//
+//  NotiData.swift
+//  Customercare
+//
+//  Created by Browser on 4/14/18.
+//  Copyright © 2018 vn. All rights reserved.
+//
+
+import Foundation
+let dataNoti=[
+    Noti(title: "meet Mr Hung", body: "Debate project 1") ,
+    Noti(title: "meet Mr Hung", body: "Debate project 2") ,
+    Noti(title: "meet Mr Hung", body: "Debate project 3") ,
+    Noti(title: "meet Mr Hung", body: "Debate project 4") ,
+    Noti(title: "meet Mr Hung", body: "Debate project 5") ,
+    Noti(title: "meet Mr Hung", body: "Debate project 6") ,
+    Noti(title: "meet Mr Hung", body: "Debate project 7") ,
+    Noti(title: "meet Mr Hung", body: "Debate project 8") ,
+    Noti(title: "meet Mr Hung", body: "Debate project 9") ,
+    Noti(title: "meet Mr Hung", body: "Debate project 10") ,
+]
